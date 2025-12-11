0640a15f6aa8bc5487b464fd10a3bb0e4cb01ffd
lonewolfyx <877085893@qq.com>
2025-12-11 23:23:12 +0800

    feat(SiteFooter): add author info and GitHub link, adjust layout to center

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -1,11 +1,21 @@
 <template>
     <footer class="group-has-[.section-soft]/body:bg-surface/40 3xl:fixed:bg-transparent dark:bg-transparent">
         <div class="container-wrapper px-4 xl:px-6">
-            <div class="flex h-(--footer-height) items-center justify-between">
-                <div class="text-muted-foreground w-full text-center text-xs leading-loose sm:text-sm">
+            <div class="flex h-(--footer-height) items-center justify-center">
+                <div class="flex flex-col items-center text-muted-foreground w-full text-xs leading-loose sm:text-sm">
                     <span>&copy; Released under the MIT License.</span>
+                    <span>Build by <NuxtLink
+                        target="_blank"
+                        to="https://github.com/lonewolfyx"
+                    >lonewolfyx</NuxtLink>. The source code is available on <NuxtLink
+                        target="_blank"
+                        to="https://github.com/lonewolfyx/shadcn-vue-hooks"
+                    >Github</NuxtLink>.</span>
                 </div>
             </div>
         </div>
     </footer>
 </template>
+
+<script setup lang="ts">
+</script>
