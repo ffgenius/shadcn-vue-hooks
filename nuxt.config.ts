8472fbf5669bd20da5de4478d9b378dbd95c0f47
lonewolfyx <877085893@qq.com>
2025-12-11 23:40:19 +0800

    feat(app): add default application header configuration, including character set, viewport, and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,16 @@ export default defineNuxtConfig({
         enabled: true,
     },
 
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width,initial-scale=1',
+            link: [
+                { rel: 'icon', href: `/favicon.ico` },
+            ],
+        },
+    },
+
     css: [
         '~/assets/css/main.css',
     ],
