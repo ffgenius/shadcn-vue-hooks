b4c3845fa277cede7dc55996500a33cadf8b4759
lonewolfyx <877085893@qq.com>
2025-12-11 21:56:21 +0800

    refactor(navigation): removes the ability to sort navigation items

--- app/composables/useNavigation.ts
+++ app/composables/useNavigation.ts
@@ -1,19 +1,3 @@
-import type { ContentNavigationItem } from '@nuxt/content'
-
-const sortChildrenByTitle = (data: ContentNavigationItem): ContentNavigationItem => {
-    if (!data.children) return data
-
-    // 对当前层级 children 排序
-    data.children = data.children.sort((a, b) => {
-        return a.title.localeCompare(b.title)
-    })
-
-    // 递归排序子层级
-    data.children = data.children.map(child => sortChildrenByTitle(child))
-
-    return data
-}
-
 export const useNavigation = async () => {
     const { data } = await useAsyncData('navigation', () => {
         return queryCollectionNavigation('content')
@@ -24,7 +8,7 @@ export const useNavigation = async () => {
 
             return [
                 {
-                    ...sortChildrenByTitle(doc),
+                    ...doc,
                 },
             ]
         },
